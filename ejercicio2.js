8cced8315a61cc47d43c76cf7073ebfac9064ab7
ferramirezmoriel <96149829+ferramirezmoriel@users.noreply.github.com>
2022-01-18 23:54:07 -0700

    DOM Manipulation
    DOM Manipulation

---
+++ ejercicio2.js
@@ -0,0 +1,7 @@
+function getFormvalue(){
+    var first = document.getElementById("fname").value;
+    var second = document.getElementById("lname").value;
+
+    console.log(first);
+    console.log(second);
+}
